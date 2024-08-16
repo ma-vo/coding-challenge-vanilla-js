95e8d25ee03a42e0e305152ef30c6a411ff3e4cc
Hendrik Neumann <hendrik.neumann@digitas.com>
2024-08-16 11:14:52 +0200

    chore: update challenge, remove solution

--- src/index.mjs
+++ src/index.mjs
@@ -1,21 +1,2 @@
-import "./styles.css";
-
-// 1. Remove the wrong animal
-// 2. Combine both arrays to a larger array called "animals"
 const cats = ["Tiger", "Lion", "Cheetah"];
 const birds = ["Elephant", "Owl", "Eagle"];
-
-const birdsCorrected = birds.filter((bird) => bird !== "Elephant");
-const animals = [...cats, ...birdsCorrected];
-
-// 3. Show the newly created array in our frontend
-const tableNode = document.getElementById("listElement");
-const tableData = tableNode.querySelector("tbody");
-
-tableData.innerHTML = ""; // Reset table data
-
-animals.forEach((animal) => {
-  const tdNode = document.createElement("tr");
-  tdNode.innerHTML = `<td>${animal}</td>`;
-  tableData.appendChild(tdNode);
-});
